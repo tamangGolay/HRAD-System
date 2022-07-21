42971645d6f9eb16a2c4ad9efb4ee5b1194db19c
tamangGolay <81210005+tamangGolay@users.noreply.github.com>
2022-07-21 10:23:26 +0600

    change

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -59,6 +59,17 @@
 
 </style>
 
+@if(count($errors) > 0)
+    <div class="alert alert-danger">
+     Upload Validation Error<br><br>
+     <ul>
+      @foreach($errors->all() as $error)
+      <li>{{ $error }}</li>
+      @endforeach
+     </ul>
+    </div>
+   @endif
+
 @if(session()->has('guesthouseself'))
 <div style="font-size:20px" class="alert alert-success text-center"> {{ session()->get('guesthouseself') }} </div> @endif
 @if(session()->has('cancel'))

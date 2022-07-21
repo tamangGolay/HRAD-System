5d8a4f64e7c42d4c71c8d6fd68d14b0cb070dd29
tashidema <tashisamten7@gmail.com>
2022-07-21 17:21:14 +0600

    view for refund report changes

--- resources/views/welfare/welfareRefundReport.blade.php
+++ resources/views/welfare/welfareRefundReport.blade.php
@@ -24,42 +24,47 @@
  <body>
  
   
-<div class="container-fluid">    
+<div class="container-fluid"  style="margin-right:20%;width:95%;">    
  <div class="row">
-        <div class="col-md-3"></div>
-                <div class="col-md-3">
-                    <div class="form-group textfont">
-                    <label class="col-md-3 col-form-label" for="stardate">Start Date</label>
-                    
+        <div class="col-md-12 col-lg-12 col-sm-12">
+                
+                    <div class="form-group row col-sm-12 col-md-12">
+                    <div class="col-lg-6 col-sm-12 col-md-6">
+                    <label class="col-md-4 col-form-label text-md-left" for="stardate">Start Date</label>
+
                         <input type="date" name="filter_startdate" id="filter_startdate" placeholder="Start Date" class="form-control" required>
-                            
+     
+                    </div>  
+						
                     
-                    <!-- <div class="form-group textfont"> -->
-                    <label class="col-md-6 col-form-label" for="stardate">End Date</label>
+                    <div class="col-lg-6 col-sm-12 col-md-6">
+                    <label class="col-md-4 col-form-label text-md-left" for="stardate">End Date</label>
+
                         <input type="date" name="filter_enddate" id="filter_enddate"  placeholder="End Date" class="form-control" required>
                        
-                    </div> 
+                    </div>
+                  </div>
                     
                     <div class="form-group textfont" align="center">
-                        <button type="button" name="filter" id="filter" class="btn btn-success">Filter</button>
+                        <button type="button" style="width:90px" name="filter" id="filter" class="btn btn-success">Filter</button>
 
-                        <button type="button" name="reset" id="reset" class="btn btn-info">Reset</button>
+                        <button type="button"  style="width:90px" name="reset" id="reset" class="btn btn-warning">Reset</button>
                     </div>
                 </div>
-                <div class="col-md-4"></div>
-            </div>
-            <br />
 
-    <div class="card-header bg-green">
+            </div>
+            
+            <br />
+<div class="card-header bg-green">
 		<div class="col text-center">
 			<h5>
-                <b>Refund Report</b>
+                <b>Welfare Refund Report</b>
             </h5>
         </div>
 	</div>   
 
    <div class="table-responsive textfont">
-    <table id="report_data" class="table table-bordered table-striped">
+    <table id="report_data" class="table table-bordered table-striped" >
         <thead>
          <tr class="text-nowrap">
 
@@ -74,8 +79,9 @@
      </table>
    </div>
      <br />
- <br />
+ <br/>
   </div>
+
  </body>
 </html>
 

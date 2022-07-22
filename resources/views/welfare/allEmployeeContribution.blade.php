6b61cd55e3c5c047b770cf295013484d0add27a5
sonam <sonamc282@gmail.com>
2022-07-22 11:50:12 +0600

    All employee contribution view
    All employee contribution view

--- resources/views/welfare/allEmployeeContribution.blade.php
+++ resources/views/welfare/allEmployeeContribution.blade.php
@@ -47,69 +47,63 @@ a {
     <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.min.js" integrity="sha384-VHvPCCyXqtD5DqJeNxl2dtTyhF78xXNXdkwX1CZeRusQfRKp+tA7hAShOK/B/fQ2" crossorigin="anonymous"></script> 
+
+
 <div class="container">
     <div class="row">
-     <div class="col-md-6 offset-md-3">
+     <div class="col-md-12 ">
             <div class="card">
-             <div class="card-header">
-                Import
+             <div class="card-header bg-green  d-flex justify-content-center">
+                 <strong>All Employee Contribuation</strong> 
              </div>
-                <div class="card-body">
+                <div class="textfont card-body">
+                    @if(count($errors) > 0)
+                        <div class="alert alert-danger">
+                                Upload Validation Error<br><br>
+                            <ul>
+                                @foreach($errors->all() as $error)
+                                <li>
+                                    {{ $error }}
+                                </li>
+                                @endforeach
+                            </ul>
+                        </div>
+                    @endif
 
-                @if(count($errors) > 0)
-    <div class="alert alert-danger">
-     Upload Validation Error<br><br>
-     <ul>
-      @foreach($errors->all() as $error)
-      <li>{{ $error }}</li>
-      @endforeach
-     </ul>
-    </div>
-   @endif
+                    @if($message = Session::get('success'))
+                        <div class="alert alert-success alert-block">
+                            <button type="button" class="close" data-dismiss="alert">×</button>
+                                <strong>
+                                 {{ $message }}
+                                </strong>
+                        </div>
+                    @endif    
+                    <form method="POST" enctype="multipart/form-data" action="{{route('import')}}" >
+                        {{csrf_field()}}
+                        <div class="form-group row">
+                            <label for="title" class="col-md-2 col-form-label text-md-right">Upload CSV/Excel</label>
+                                <div class="col-sm-10 col-md-6 col-lg-10 ">
+                                    <input type="file" name="file" class="form-control">
+                                </div>
+                        </div>
 
-
-
-   @if($message = Session::get('success'))
-   <div class="alert alert-success alert-block">
-    <button type="button" class="close" data-dismiss="alert">×</button>
-           <strong>{{ $message }}</strong>
-           </div>
-   @endif    
-<form method="POST" enctype="multipart/form-data" action="{{route('import')}}" >
-    {{csrf_field()}}
-    <div class="form-group">
-    <label for="title">Choose CSV</label>
-    <input type="file" name="file" class="form-control">
-    </div>
-<input type="submit"name="upload" value="upload"class="btn btn-primary"> Submit</button> 
-</form>
+                        <div class="form-group row mb-0">
+						    <div class="col text-center col-form-label col-md-center col-sm-2 col-md-10 col-lg-12 ">
+                                <input type="submit"name="upload" value="upload"class="btn btn-outline-primary btn-md"></button>
+						    </div>
+					    </div>    
+                    </form>
                 </div>
             </div>
         </div>
     </div>
-
-</div>
-
-
-
-
-    
+</div> 
 
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.19.0/jquery.validate.js"></script>
     <script src="https://cdn.datatables.net/1.10.16/js/jquery.dataTables.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
     <!-- <script src="https://cdn.datatables.net/1.10.19/js/dataTables.bootstrap4.min.js"></script>   -->
-<script type="text/javascript">
-  
-  
-
-
-  
-
-
-     
-     
-</script>
+    <script type="text/javascript">  </script>
 
 <script src="{{asset('assets/js/jquery-3.5.1.slim.min.js')}}"></script>
 		<script type="text/javascript">

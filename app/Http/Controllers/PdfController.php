bdd0eed1dd7f84f934eaf5269e85ff01f84cdaae
tamangGolay <81210005+tamangGolay@users.noreply.github.com>
2022-07-29 09:57:32 +0600

    changes
    changes

--- app/Http/Controllers/PdfController.php
+++ app/Http/Controllers/PdfController.php
@@ -3,15 +3,30 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\notesheetRequest;
+use App\notesheetapprove;
+use DB;
+
 use PDF;
+// use Barryvdh\DomPDF\Facade\Pdf;
+use App\Models\User;
+
 
 class PdfController extends Controller
 {
-    public function create()
-    {
-    	$data = ['title' => 'Laravel 7 Generate PDF From View Example Tutorial'];
-        $pdf = PDF::loadView('pdf', $data);
-  
-        return $pdf->download('Nicesnippets.pdf');
+     public function createPDF ($id) {
+
+
+    $notesheetapprove = notesheetapprove::all()->where('noteId',$id);
+    $notesheet = notesheetRequest::find($id);
+    //For officeName in the report(pdf)
+    $date = DB::table('notesheet1')
+    ->join('officedetails','officedetails.id','=','notesheet1.officeId')
+       ->select('*')	
+        ->where('notesheet1.id',$id)
+        ->first();
+
+        $pdf = PDF ::loadView ('Notesheet.index', array('date'=>$date,'notesheet'=>$notesheet,'notesheetapprove'=>$notesheetapprove));
+        return $pdf->download ('notesheet.pdf');
     }
 }

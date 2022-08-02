0e52112b590b907161e92c36ef67fdfe260e719e
tamangGolay <81210005+tamangGolay@users.noreply.github.com>
2022-08-02 09:55:33 +0600

    changes
    changes

--- config/mail.php
+++ config/mail.php
@@ -12,8 +12,9 @@ return [
     | and used as needed; however, this mailer will be used by default.
     |
     */
+    
 
-    'default' => env('MAIL_MAILER', 'smtp'),
+    'default' => env('MAIL_DRIVER', 'smtp'),
 
     /*
     |--------------------------------------------------------------------------

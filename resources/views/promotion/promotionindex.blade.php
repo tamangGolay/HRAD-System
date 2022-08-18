5fce48234777938d62bf2a07008427f925f7f3bb
Leki Yangdon <lyangdon22@gmail.com>
2022-08-18 16:32:05 +0600

    msg

--- resources/views/promotion/promotionindex.blade.php
+++ resources/views/promotion/promotionindex.blade.php
@@ -5,12 +5,28 @@
     <meta charset = "UTF-8">
     <meta http-equiv = "X-UA-Compatible" content = "IE = edge">
     <meta name = "viewport" content = "width = device-width, initial-scale = 1.0">
-    <title> Laravel 8 PDF </title>
+    <title> Promomtion Report</title>
     <! - Bootstrap5 CSS ->
    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
 
    <style>
-   
+
+
+
+a {
+        color: black !important;
+        text-decoration: none;
+    }
+    
+    
+ 
+    .alert-success {
+      color: #28a745 !important;
+        background-color: #28a745 !important;
+        border-color: #28a745 !important;
+}
+
+
  table, th, td {
   border-color: #FFFFFF;
  

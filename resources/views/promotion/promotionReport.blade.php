b42fcedad07d0418fd0de07e5ff1a0cefc4f7b94
Leki Yangdon <lyangdon22@gmail.com>
2022-08-18 16:41:35 +0600

    css

--- resources/views/promotion/promotionReport.blade.php
+++ resources/views/promotion/promotionReport.blade.php
@@ -9,6 +9,18 @@
    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
 
    <style>
+    a {
+        color: black !important;
+        text-decoration: none;
+    }
+    
+    
+ 
+    .alert-success {
+      color: #fff !important;
+        background-color: #28a745 !important;
+        border-color: #28a745 !important;
+}
    table, th, td {
   border: 1px solid black;
 }

64d6227ba0073b35de55e9a63c530cc19cb6659c
tashidema <tashisamten7@gmail.com>
2022-08-23 11:15:17 +0600

    i

--- app/WfReleaseProcess.php
+++ app/WfReleaseProcess.php
@@ -9,7 +9,9 @@ class WfReleaseProcess extends Model
     //
     protected $table = 'wfreleaseprocess';
 
-    protected $fillable = [ 'id','empId','requestDate','amount','deathOf','reason','status','createdBy','member1Id','member1Action','member1ActionDate','member2id','member2Action','member2ActionDate','chairAction','chairEmpId','chairActionDate'];
+    protected $fillable = [ 'id','empId','requestDate','amount','deathOf',
+    'reason','status','createdBy','member1Id','member1Action','member1ActionDate',
+    'member2Id','member2Action','member2ActionDate','chairAction','chairEmpId','chairActionDate'];
     public $timestamps = false;
 
 }

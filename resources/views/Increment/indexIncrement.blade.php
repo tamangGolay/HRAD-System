dabe2bea91ead27311b009881fef2a276249f4a0
tashidema <tashisamten7@gmail.com>
2022-09-02 16:21:14 +0600

    align

--- resources/views/Increment/indexIncrement.blade.php
+++ resources/views/Increment/indexIncrement.blade.php
@@ -17,41 +17,25 @@
   border-color: #000000;
  
 }
-
-
-div.notesheet {
-  position: absolute;
-  right: 45;
-  width: 100px;
-  height: 120px;
-  }
-
-
    </style>
+
 </head>
 <body>
 
   <div class = "container-fluid mt-4">
-
         <div class = "row">
-        <img src="{{asset('/cd/images/header.jpg')}}" width="100%" height="100px">
-        <br><br><br>
-        <div class = "col-md-8">
-            <div class="notesheet">{{date('d-m-Y', strtotime($increment1->createdOn))}}</div> 
+            <img src="{{asset('/cd/images/header.jpg')}}" width="100%" height="100px">
+            <br><br><br>
+            <div class = "col-md-8">
+                <div class="notesheet">{{date('d-m-Y', strtotime($increment1->createdOn))}}</div> 
                 <p> BPC/HRAD/HRMD-05/{{$increment1->incrementDate}} / {{$increment1->id}} </p>
             </div>
-
-        
-
             <br>
             <div class ="row">
                 <div class = "col-md-12">
-
                     <p>{{$headDesignation->designation}}</p>
                     <p>{{$increment1->longOfficeName}}</p>
                     <p>{{$increment1->officeAddress}}</p>
-
-
                 </div>
             </div>
             <br>
@@ -71,81 +55,75 @@ div.notesheet {
                 <div class = "col-md-12 increment1">
                 <p> The final increment payout for the {{$increment1->incrementCycle}} Cycle {{$increment1->incrementDate}} is circulated for kind information and necessary action. </p>
                 </div>
-            </div>
-        
+            </div>     
             <div class = "row">
-            <div class = "col-md-12">
-            
-              <div class="card-body table-responsive p-0">
-                <table class="table table-hover table-striped table-bordered gg">
-
-                <thead>
-                            <tr>
-                            <th scope="col">SN</th>
-                            <th scope="col">Name(Employee Id)</th>
-                            <th scope="col">Designation(Grade)</th>
-                            <th scope="col">Old Basic</th>
-                            <th scope="col">increment</th>
-                            <th scope="col">New Basic</th>
+                <div class = "col-md-12">      
+                    <div class="card-body table-responsive p-0">
+                        <table class="table table-hover table-striped table-bordered gg">
+                            <thead>
+                                <tr>
+                                    <th scope="col">SN</th>
+                                    <th scope="col">Name(Employee Id)</th>
+                                    <th scope="col">Designation(Grade)</th>
+                                    <th scope="col">Old Basic</th>
+                                    <th scope="col">increment</th>
+                                    <th scope="col">New Basic</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                            <tr>             
+                             @foreach ($increment as $increment)             
+                                <td class="col2"> {{$loop->iteration}}</td>
+                                <td class="col2"> {{$increment->empName }} ({{$increment->empId}}) </td>
+                                <td class="col2"> {{$increment->designation}}({{$increment->grade}}) </td>
+                                <td class="col2"> {{$increment-> oldBasic}} </td>
+                                <td class="col2"> {{$increment-> increment}} </td>
+                                <td class="col2"> {{$increment-> newBasic}} </td>
                             </tr>
-                        </thead>
-
-                        <tbody>
-                        <tr>             
-
-                        @foreach ($increment as $increment)             
-                           <td class="col2"> {{$loop->iteration}}</td>
-                          <td class="col2"> {{$increment->empName }} ({{$increment->empId}}) </td>
-                          <td class="col2"> {{$increment->designation}}({{$increment->grade}}) </td>
-                           <td class="col2"> {{$increment-> oldBasic}} </td>
-                            <td class="col2"> {{$increment-> increment}} </td>
-                            <td class="col2"> {{$increment-> newBasic}} </td>
-                        </tr>
-                        </tbody>
-                        @endforeach
-
-                  </table>
-</div> </div> </div>
-
-                  <br>
-                  <div class = "row">
-                   <div class = "col-md-12 ">
-                    <p> You are requested to inform the employees under their jurisdiction of the same and also to maintain a copy of the same in their personal file for record. </p> </div>
+                            </tbody>
+                             @endforeach
+                        </table>
+                    </div> 
                 </div> 
-                <br>
-                <div class = "row">
-                   <div class = "col-md-12 ">
-                    <p> Yours Sincerely </p></div>
+            </div>
+            <br>
+            <div class = "row">
+                <div class = "col-md-12 ">
+                    <p> You are requested to inform the employees under their jurisdiction of the same and also to maintain a copy of the same in their personal file for record. </p> 
                 </div>
-                <br>
-
-                <div class = "row">
-                   <div class = "col-md-12 increment1">
+            </div> 
+            <br>
+            <div class = "row">
+                <div class = "col-md-12 ">
+                    <p> Yours Sincerely </p>
+                </div>
+            </div>
+            <br>
+            <div class = "row">
+                <div class = "col-md-12 increment1">
                    <img src="{{asset('/cd/images/hrsign.gif')}}" width="20%" height="30px">
-                   <br>
-                    
+                   <br>       
                     <p> {{$GmName-> empName }} </p>
                     <p> General Manager-HR </p>
-                    </div>
-                </div> <br><br>
-                <br>                <div class = "row">
-                   <div class = "col-md-12 increment1">
+                </div>
+            </div> 
+            <br><br><br>                
+            <div class = "row">
+                <div class = "col-md-12 increment1">
                     <p> Copy to: </p>
                     <p> &nbsp; &nbsp; 1. {{$PiadDesignation->desisnamelong }}, PIAD, HRAD, BPC for further necessary action.  </p>                    
                     <p> &nbsp; &nbsp; 2. HR Admin, {{$officeAddress->officeDetails }} for necessary action. </p>
                     <p> &nbsp; &nbsp; 3. Person Concern</p>
-
-                    </div>
                 </div>
             </div>
-            </div>
+        </div>
+            
        
-  <div class = "row">
+    <div class = "row">
         <!-- <img src="{{asset('/cd/images/footer.jpg')}}" width="40%" height="100px"> -->
         <img src="{{asset('/cd/images/footer.PNG')}}" width="80%" height="40px">
-
+    </div>
 </div>
-
 
       
    
@@ -156,7 +134,7 @@ div.notesheet {
 
     <script src = "https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"> </script>
    
-</body>
+    </body>
 </html>
 
 

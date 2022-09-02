9f4d4a53abce97ada2303e8f41ce561727969541
tashilhamo55 <83581317+tashilhamo55@users.noreply.github.com>
2022-09-02 15:01:16 +0600

    d

--- resources/views/Increment/incrementReport.blade.php
+++ resources/views/Increment/incrementReport.blade.php
@@ -105,6 +105,9 @@ h1 {
         </div>
     </div>
 
+    <div style="page-break-before:always;">
+   
+
     <script src="{{asset('assets/js/jquery-3.5.1.slim.min.js')}}"></script>
     <script src = "https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity = "sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KinkN" crossorigin="anonymous"></script>
    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
--- resources/views/Notesheet/index.blade.php
+++ resources/views/Notesheet/index.blade.php
@@ -105,8 +105,7 @@
             </div>
         </div>
     </div>
-    <div style="page-break-before:always;">
-   
+    
     <script src = "https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity = "sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KinkN" crossorigin="anonymous"></script>
    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>

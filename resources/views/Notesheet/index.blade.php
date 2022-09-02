cc65d3a6c04cee97f611ec7e14069185c473dafe
tashilhamo55 <83581317+tashilhamo55@users.noreply.github.com>
2022-09-02 10:47:54 +0600

    note

--- resources/views/Notesheet/index.blade.php
+++ resources/views/Notesheet/index.blade.php
@@ -69,7 +69,7 @@ div.notesheet {
                       </tr>
                       <tr >
                         <th scope = "col" class="col5"> Justification </th>
-                        <td class="col1 align-top"> {!! nl2br ($notesheet-> justification) !!} </td>
+                        <td class="col1" style="vertical-align:text-top"> {!! nl2br ($notesheet-> justification) !!} </td>
                       
                       </tr>
                       <tr>

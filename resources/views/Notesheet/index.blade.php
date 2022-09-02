d65b312215b187354e0f3bb7bb3898a50bdc561c
tashilhamo55 <83581317+tashilhamo55@users.noreply.github.com>
2022-09-02 14:53:58 +0600

    page break for increment

--- resources/views/Notesheet/index.blade.php
+++ resources/views/Notesheet/index.blade.php
@@ -105,6 +105,7 @@
             </div>
         </div>
     </div>
+    <div style="page-break-before:always;">
    
     <script src = "https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity = "sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KinkN" crossorigin="anonymous"></script>
    <!-- jQuery first, then Popper.js, then Bootstrap JS -->

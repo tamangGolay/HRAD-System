b2c7078c8c0d1dcd9802d8c7bc61700fd4dc584e
tashidema <tashisamten7@gmail.com>
2023-03-09 10:53:22 +0600

    changes

--- app/Http/Controllers/OfficeWiseUniformSizeReportController.php
+++ app/Http/Controllers/OfficeWiseUniformSizeReportController.php
@@ -24,7 +24,7 @@ class OfficeWiseUniformSizeReportController extends Controller
          ->select('*')            
          ->where('Office','=',$request->officeId)
          ->where('cloths','=',$request->cloths)
-
+         ->where('officeuniform.size','!=','Not Applicable')
         //  ->where('employeeuniform.status','=', 0)
 
          ->get();

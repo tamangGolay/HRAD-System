d87530373903deb8ecee49ce7aed76a9f3f60347
tashidema <tashisamten7@gmail.com>
2025-04-01 12:54:40 +0600

    check double entry

--- app/Http/Controllers/LaptopReleaseController.php
+++ app/Http/Controllers/LaptopReleaseController.php
@@ -13,18 +13,22 @@ class LaptopReleaseController extends Controller
 
     public function Laptop_release(Request $request)
     {
+
+        $existing = LaptopRelease::where('empid', $request->EmpId)->first();
+        if (!$existing) {
         
             $Request_release = new LaptopRelease;
             $Request_release->empid = $request->EmpId;    
             $Request_release->remarks = $request->remarks;
-            $Request_release->save();    
-
+            $Request_release->save();  
 
             return redirect('home')->with('page', 'laptopdetails')
                 ->with('success', 'You have successfully saved the record!');
 
         } 
+        return redirect('home')->with('page', 'laptopdetails')
+                ->with('error', 'This EmpID is already assigned for co-purchase.');
 
     
-}
+}}
 

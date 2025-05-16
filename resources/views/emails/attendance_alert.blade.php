5a2814f4a990c0b138bf7bb534cf6f71f4b08676
tashidema <tashisamten7@gmail.com>
2025-05-16 16:54:01 +0600

    attendance email

---
+++ resources/views/emails/attendance_alert.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Attendance Report</title>
+</head>
+<body>
+    <p>Hello,</p>
+    <p>The monthly attendance report is attached.</p>
+    <p>Please review it.</p>
+    <p>Best Regards,</p>
+    <p>BPC Atttendance System</p><br>
+    <p style="text-align:center"><i>***This is system generated report.***</i></p>
+</body>
+</html>
---
+++ resources/views/emails/attendancemail.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+</head>
+<body>
+    <h1>{{ $details['title'] }}</h1><br>
+    <p>{{ $details['body'] }}</p>
+    <p>{!! $details['body1'] !!}</p>
+    <p>{!! $details['body2'] !!}</p>
+    <p>{!! $details['body3'] !!}</p><br>  
+      
+    <p><i>{!! $details['body4'] !!}</i></p><br>
+    <p>Thank You</p> 
+    
+    <p>(This is an auto-generated response. Please do not reply to this mail)</p>
+
+</body>
+</html>
